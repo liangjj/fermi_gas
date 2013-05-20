1a6c23b787bb8d8d3baeb6f326620a715ff907c2
liangjj <liangjj@sxu.edu.cn>
2013-05-20 22:19:19 +0800

    mod function is added

--- complex.cpp
+++ complex.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <cmath>
 class Complex
 {
 public:
  Complex(){real=0;imag=0;}
  Complex(double r,double i){real=r;imag=i;}
- Complex operator+(Complex& c2); //复数相加
- Complex operator*(Complex& c2);  //复数相乘
- Complex operator-(Complex& c2); //复数相减
- Complex operator/(Complex& c2); //复数相除
+ Complex operator+(Complex& c2); // add
+ Complex operator*(Complex& c2);  // product
+ Complex operator-(Complex& c2); // minus
+ Complex operator/(Complex& c2); //
+ Complex operator+=(Complex& c2); // doesnot work well
  friend std::ostream &operator<<(std::ostream &os,const Complex &c);
+//the module of complex 
+ double mod();
 private:
  double real;
  double imag;
@@ -32,20 +36,29 @@ double mod = c2.real*c2.real+c2.imag*c2.imag;
 return Complex((real*c2.real+imag*c2.imag)/mod,(imag*c2.real-real*c2.imag)/mod);
 }
 
+Complex Complex::operator+=(Complex& c2){
+return Complex(real+c2.real,imag+c2.imag);
+}
+
 std::ostream & operator<<(std::ostream &os,const Complex &c)
 {
     os << c.real;
     if(c.imag > 0)
         os << "+" << c.imag << "i" << std::endl;
     else
-        os << c.imag << "i" << std::endl;
+        os << "+" << c.imag << "i" << std::endl;
     return os;
+}
+
+double Complex::mod() {
+return std::sqrt(real*real+imag*imag);
 }
 
 int main() {
 Complex c1(1,2),c2(2,3),c3;
-c3 = c1 + c2;
+c3 += c1;
 std::cout << "hello" << std::endl;
-std::cout << c3 << std::endl;
+std::cout << c3  << std::endl;
+std::cout <<"the module of c1 is " << c1.mod() << std::endl;
 return 0;
 }
