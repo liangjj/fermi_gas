b224e4a93b9c7e0fb8511ee03e516438ebe565e8
liangjj <liangjj@sxu.edu.cn>
2013-05-21 18:38:37 +0800

    operator += is added

--- complex.cpp
+++ complex.cpp
@@ -1,3 +1,5 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
 #include <iostream>
 #include <cmath>
 class Complex
@@ -5,11 +7,12 @@ class Complex
 public:
  Complex(){real=0;imag=0;}
  Complex(double r,double i){real=r;imag=i;}
- Complex operator+(Complex& c2); // add
- Complex operator*(Complex& c2);  // product
- Complex operator-(Complex& c2); // minus
- Complex operator/(Complex& c2); //
- Complex operator+=(Complex& c2); // doesnot work well
+ friend Complex operator+(Complex& c1, Complex& c2); // add
+ friend Complex operator*(Complex& c1, Complex& c2);  // product
+ friend Complex operator-(Complex& c1, Complex& c2); // minus
+ friend Complex operator/(Complex& c1, Complex& c2); //
+ //Complex operator=(Complex& c);// doesnot work well
+ Complex operator+=(Complex& c2); 
  friend std::ostream &operator<<(std::ostream &os,const Complex &c);
 //the module of complex 
  double mod();
@@ -18,35 +21,36 @@ private:
  double imag;
 };
 
-Complex Complex::operator+(Complex& c2)
+Complex operator+(Complex& c1, Complex& c2)
 {
- return Complex(real+c2.real,imag+c2.imag);
+ //return Complex(real+c2.real,imag+c2.imag); //
+ return Complex(c1.real+c2.real,c1.imag+c2.imag);
 }
 
-Complex Complex::operator*(Complex& c2)
+Complex operator*(Complex& c1, Complex& c2)
 {
-return Complex (real*c2.real-imag*c2.imag,real*c2.imag+imag*c2.real);
+return Complex (c1.real*c2.real-c1.imag*c2.imag,c1.real*c2.imag+c1.imag*c2.real);
 }
 
-Complex Complex::operator-(Complex& c2){
-return Complex(real-c2.real,imag-c2.imag);
+Complex operator-(Complex& c1, Complex& c2){
+return Complex(c1.real-c2.real,c1.imag-c2.imag);
 }
-Complex Complex::operator/(Complex& c2){
+Complex operator/(Complex& c1, Complex& c2){
 double mod = c2.real*c2.real+c2.imag*c2.imag;
-return Complex((real*c2.real+imag*c2.imag)/mod,(imag*c2.real-real*c2.imag)/mod);
+return Complex((c1.real*c2.real+c1.imag*c2.imag)/mod,(c1.imag*c2.real-c1.real*c2.imag)/mod);
 }
 
 Complex Complex::operator+=(Complex& c2){
-return Complex(real+c2.real,imag+c2.imag);
+return Complex(real+=c2.real,imag+=c2.imag);
 }
+
+//Complex Complex::operator=(Complex& c){
+//return Complex(c.real,c.imag);  //why not: return Complex(c.real,c.imag); doesnot work well
+//}
 
 std::ostream & operator<<(std::ostream &os,const Complex &c)
 {
-    os << c.real;
-    if(c.imag > 0)
-        os << "+" << c.imag << "i" << std::endl;
-    else
-        os << "+" << c.imag << "i" << std::endl;
+    os << c.real << "+" << c.imag << "i" << std::endl;
     return os;
 }
 
@@ -56,9 +60,11 @@ return std::sqrt(real*real+imag*imag);
 
 int main() {
 Complex c1(1,2),c2(2,3),c3;
-c3 += c1;
-std::cout << "hello" << std::endl;
-std::cout << c3  << std::endl;
+c3 = c1+c2;
+std::cout << "c1= " << c1 << std::endl;
+std::cout << "c2= " << c2 << std::endl;
+std::cout << "c3= " << c3 << std::endl;
 std::cout <<"the module of c1 is " << c1.mod() << std::endl;
 return 0;
 }
+#endif
